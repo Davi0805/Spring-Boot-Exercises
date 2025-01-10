700a33b65bdc36fed63792b3712321e9bf696506
Davi0805 <melocardozo.video@gmail.com>
2025-01-10 02:05:06 +0000

    Interface BookRep JPA SEM OVERRIDE

---
+++ spring00/src/main/java/com/example/spring/repository/BookRepository.java
@@ -0,0 +1,8 @@
+package com.example.spring.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.spring.model.Book;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+}

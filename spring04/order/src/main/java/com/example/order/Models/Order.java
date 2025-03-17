dbd32d0df3f48e825c9a92708256396e78de3dca
Davi0805 <melocardozo.video@gmail.com>
2025-03-17 15:02:09 +0000

    feat(Model): Order entity created

---
+++ spring04/order/src/main/java/com/example/order/Models/Order.java
@@ -0,0 +1,30 @@
+package com.example.order.Models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ElementCollection
+    private List<Product> products;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    private LocalDateTime created_at;
+}

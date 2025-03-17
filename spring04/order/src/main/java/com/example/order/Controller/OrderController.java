0652ad51d210ca2c4937bdb0b700313d88043df4
Davi0805 <melocardozo.video@gmail.com>
2025-03-17 15:28:10 +0000

    feat(Controller): Post and getAll endpoints implemented

---
+++ spring04/order/src/main/java/com/example/order/Controller/OrderController.java
@@ -0,0 +1,49 @@
+package com.example.order.Controller;
+
+import com.example.order.Models.Order;
+import com.example.order.Services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+
+    private final OrderService cursor;
+
+    @Autowired
+    public OrderController(OrderService cursor)
+    {
+        this.cursor = cursor;
+    }
+
+    @PostMapping
+    public ResponseEntity<UUID> createOrder(Order order)
+    {
+        try {
+            UUID orderId = cursor.createOrder(order);
+            return ResponseEntity.ok(orderId);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Order>> getAllOrders()
+    {
+        try {
+            return ResponseEntity.ok(cursor.getAllOrders());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return ResponseEntity.notFound().build();
+        }
+    }
+}

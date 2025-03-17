4a4992dfc47b48073631d642ef20b7ddc665bb7f
Davi0805 <melocardozo.video@gmail.com>
2025-03-17 13:23:23 +0000

    refac(OrderService): Implemented checks that product quantity cant be 0 and use the constructor and return the id of order

--- spring03/order-ms/src/main/java/com/example/orderms/Services/OrderService.java
+++ spring03/order-ms/src/main/java/com/example/orderms/Services/OrderService.java
@@ -2,10 +2,12 @@ package com.example.orderms.Services;
 
 import com.example.grpc.product.ProductRequest;
 import com.example.orderms.Models.Order;
+import com.example.orderms.Models.Product;
 import com.example.orderms.Repository.OrderRepository;
 import org.apache.coyote.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -24,33 +26,47 @@ public class OrderService {
         this.productGrpcService = productGrpcService;
     }
 
-    public Order createOrder(Order order) throws BadRequestException {
+    @Transactional
+    public UUID createOrder(Order order) throws BadRequestException {
 
-        for (String productId : order.getProducts()) {
-            if (!productGrpcService.checkProductStock(productId, 1)) {
+        // O @Valid nao esta funcionando em Product
+        // Provavelmente pq era Embeddable
+        for (Product product : order.getProducts())
+        {
+            if (product.getQuantity() <= 0)
+                throw new BadRequestException("Nao pode existir um produto com quantidade 0");
+        }
+
+        // Poderia simplesmente receber um DTO no endpoint
+        Order newOrder = new Order(order.getProducts());
+
+        for (Product productId : newOrder.getProducts()) {
+            if (!productGrpcService.checkProductStock(productId.getProductId(), productId.getQuantity())) {
                 throw new BadRequestException("Produto sem estoque, id: " + productId);
             }
         }
 
         boolean stockUpdated = productGrpcService.updateProductStock(
-                order.getId().toString(),
-                order.getProducts().stream()
+                newOrder.getId().toString(),
+                newOrder.getProducts().stream()
                         .map(productId -> ProductRequest.newBuilder()
-                                .setProductId(productId)
-                                .setQuantity(1)
+                                .setProductId(productId.getProductId())
+                                .setQuantity(productId.getQuantity())
                                 .build())
                         .collect(Collectors.toList())
         );
 
+        // Em caso de race condition
         if (!stockUpdated) {
             throw new BadRequestException("Falha ao atualizar estoque");
         }
 
-        return cursor.save(order);
+        cursor.save(newOrder);
+
+        return newOrder.getId();
     }
 
     public Order getOrderById(UUID id) throws BadRequestException {
-        productGrpcService.testGrpc();
 
         return cursor.findById(id).orElseThrow(() -> new BadRequestException("Order not found"));
     }

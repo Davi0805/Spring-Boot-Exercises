24589b8af7c9012cf92557b4e605ca2cdf9850c2
Davi0805 <melocardozo.video@gmail.com>
2025-03-17 20:07:56 +0000

    feat(Kafka): Logica para setar status do pedido em order como NOTIFIED

--- spring04/notification/src/main/java/com/example/notification/Kafka/Consumer/KafkaMessageConsumer.java
+++ spring04/notification/src/main/java/com/example/notification/Kafka/Consumer/KafkaMessageConsumer.java
@@ -1,21 +1,23 @@
 package com.example.notification.Kafka.Consumer;
 
+import com.example.notification.Kafka.Producer.MessageProducer;
 import com.example.notification.Models.Order;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
-
 @Component
 public class KafkaMessageConsumer {
 
     private final ObjectMapper desserializer;
+    private final MessageProducer messageProducer;
 
     @Autowired
-    public KafkaMessageConsumer(ObjectMapper desserializer)
+    public KafkaMessageConsumer(ObjectMapper desserializer, MessageProducer producer)
     {
         this.desserializer = desserializer;
+        this.messageProducer = producer;
     }
 
     @KafkaListener(topics = "emailNotification", groupId = "notification-group")
@@ -29,6 +31,7 @@ public class KafkaMessageConsumer {
         try {
             Order order = desserializer.readValue(message, Order.class);
             System.out.println("Order deserializado: " + order);
+            messageProducer.sendMessage("emailNotified", String.valueOf(order.getId()));
         } catch (Exception e) {
             throw new RuntimeException(e.getMessage());
         }
---
+++ spring04/order/src/main/java/com/example/order/Kafka/KafkaMessageConsumer.java
@@ -0,0 +1,40 @@
+package com.example.order.Kafka;
+
+import com.example.order.Models.Order;
+import com.example.order.Repository.OrderRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+import com.example.order.Kafka.MessageProducer;
+
+import java.util.UUID;
+
+import static com.example.order.Models.OrderStatus.NOTIFIED;
+
+@Component
+public class KafkaMessageConsumer {
+
+    private final ObjectMapper desserializer;
+    private final MessageProducer messageProducer;
+    private final OrderRepository cursor;
+
+    @Autowired
+    public KafkaMessageConsumer(ObjectMapper desserializer, MessageProducer producer, OrderRepository orderRepo)
+    {
+        this.desserializer = desserializer;
+        this.messageProducer = producer;
+        this.cursor = orderRepo;
+    }
+
+    @KafkaListener(topics = "emailNotified", groupId = "notification-group")
+    public void listen(String message)
+    {
+        System.out.println("Mensagem recebida no Consumer: " + message);
+
+        //LOGICA DE EMAIL OU NOTIFICACAO ACONTECERIA AQUUI
+        Order order = cursor.findById(UUID.fromString(message)).orElseThrow(() -> new RuntimeException());
+        order.setStatus(NOTIFIED);
+        cursor.save(order);
+    }
+}

6aa64a9dcdc352134f420615346102cfd950b39c
Davi0805 <melocardozo.video@gmail.com>
2025-03-18 17:16:20 +0000

    feat(product-service/Repository): DecrementStock custom query

--- spring05/product-service/src/main/java/com/example/productservice/Repository/ProductRepository.java
+++ spring05/product-service/src/main/java/com/example/productservice/Repository/ProductRepository.java
@@ -2,10 +2,21 @@ package com.example.productservice.Repository;
 
 import com.example.productservice.Models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, UUID> {
+
+
+    // TODO: ANALISAR COMO ATUALIZAR LAST PIECES QUANDO CHEGAR EM UM NUMEOR ESPECIFICO
+    @Transactional
+    @Modifying
+    @Query("UPDATE Product p SET p.quantity = p.quantity - :quantity WHERE p.id = :productId AND p.quantity >= :quantity")
+    int decrementStock(UUID productId, int quantity);
+
 }

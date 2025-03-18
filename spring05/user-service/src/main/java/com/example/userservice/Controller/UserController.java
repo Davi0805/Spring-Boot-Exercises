efc8d03192ad36dc457335bf799dd968e4ca7eba
Davi0805 <melocardozo.video@gmail.com>
2025-03-18 05:08:17 +0000

    feat(user-service/Controller): added me endpoint and extract token login to extract to futures auths

--- spring05/user-service/src/main/java/com/example/userservice/Controller/UserController.java
+++ spring05/user-service/src/main/java/com/example/userservice/Controller/UserController.java
@@ -2,25 +2,30 @@ package com.example.userservice.Controller;
 
 import com.example.userservice.DTO.FrontEndMetadataDTO;
 import com.example.userservice.DTO.LoginDTO;
+import com.example.userservice.DTO.PersonalDataDTO;
 import com.example.userservice.Models.User;
 import com.example.userservice.Redis.JwtSession;
 import com.example.userservice.Service.UserService;
 import jakarta.transaction.Transactional;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
+
+import static org.springframework.boot.actuate.web.exchanges.Include.AUTHORIZATION_HEADER;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
     // TODO: LOGICA ROBUSTA DE LOG
+
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+    private static final String BEARER_PREFIX = "Bearer ";
 
     private final UserService service;
 
@@ -51,5 +56,23 @@ public class UserController {
             System.out.println(e.getMessage());
             return ResponseEntity.badRequest().body("Erro: Falha ao criar usuario");
         }
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<PersonalDataDTO> getMe(@RequestHeader(AUTHORIZATION_HEADER) String bearerToken) {
+        try {
+            String token = extractToken(bearerToken);
+            return ResponseEntity.ok(service.getMyUser(token));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    private String extractToken(String bearerToken) {
+        if (bearerToken != null && bearerToken.startsWith(BEARER_PREFIX)) {
+            return bearerToken.substring(BEARER_PREFIX.length());
+        }
+        throw new RuntimeException("Invalid Authorization header");
     }
 }

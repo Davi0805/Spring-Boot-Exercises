31ad5c9ca5218238e39bd1942560e70df8b2fff8
Davi0805 <melocardozo.video@gmail.com>
2025-03-18 23:21:54 +0000

    feat(Order-Service/Services): updatestock added to GrpcOrderClient

--- spring05/Order-Service/src/main/java/com/example/orderservice/Service/grpcOrderClient.java
+++ spring05/Order-Service/src/main/java/com/example/orderservice/Service/grpcOrderClient.java
@@ -1,12 +1,14 @@
 package com.example.orderservice.Service;
 
-import com.example.grpc.product.ProductRequest;
-import com.example.grpc.product.ProductResponse;
-import com.example.grpc.product.ProductServiceGrpc;
+import com.example.grpc.product.*;
 import com.example.grpc.product.ProductServiceGrpc.*;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.example.orderservice.Models.Product;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class grpcOrderClient {
@@ -14,12 +16,27 @@ public class grpcOrderClient {
     @GrpcClient("product-service")
     private ProductServiceBlockingStub productServiceBlockingStub;
 
-    public boolean checkStock(String id, int quantity)
+    public ProductResponse checkStock(String id, int quantity)
     {
         ProductRequest request = ProductRequest.newBuilder()
                 .setProductId(id).setQuantity(quantity).build();
 
         ProductResponse response = productServiceBlockingStub.checkStock(request);
-        return response.getInStock();
+        return response;
+    }
+
+    public boolean updateStock(String id, List<Product> products)
+    {
+        List<ProductRequest> productsList = new ArrayList<>();
+        for (Product produto : products)
+        {
+            productsList.add(ProductRequest.newBuilder().setProductId(String.valueOf(produto.getId())).setQuantity(produto.getQuantity()).build());
+        }
+
+        OrderRequest request = OrderRequest.newBuilder().setOrderId(id).addAllProducts(productsList).build();
+
+        OrderResponse response = productServiceBlockingStub.updateStock(request);
+
+        return response.getSuccess();
     }
 }

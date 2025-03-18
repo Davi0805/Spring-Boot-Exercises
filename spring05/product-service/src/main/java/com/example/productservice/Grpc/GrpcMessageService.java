0e75330540f916c58cd0ffa48118fec88dc9e6dd
Davi0805 <melocardozo.video@gmail.com>
2025-03-18 16:47:25 +0000

    feat(product-service/GrpcMessageService): checkStock server implemented

---
+++ spring05/product-service/src/main/java/com/example/productservice/Grpc/GrpcMessageService.java
@@ -0,0 +1,50 @@
+package com.example.productservice.Grpc;
+
+import com.example.grpc.product.ProductRequest;
+import com.example.grpc.product.ProductResponse;
+import com.example.grpc.product.ProductServiceGrpc;
+import com.example.productservice.Models.Product;
+import com.example.productservice.Repository.ProductRepository;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.UUID;
+
+@GrpcService
+public class GrpcMessageService extends ProductServiceGrpc.ProductServiceImplBase {
+
+    private final ProductRepository cursor;
+
+    @Autowired
+    public GrpcMessageService(ProductRepository repo)
+    {
+        this.cursor = repo;
+    }
+
+    @Override
+    public void checkStock(ProductRequest request, StreamObserver<ProductResponse> responseObserver)
+    {
+        ProductResponse response;
+        try {
+            Product temp = cursor.findById(UUID.fromString(request.getProductId()))
+                                .orElseThrow(() -> new RuntimeException("Produto nao encontrado"));
+
+            if (temp.getQuantity() < request.getQuantity() && request.getQuantity() > 0)
+                throw new RuntimeException("Estoque em falta");
+
+            response = ProductResponse.newBuilder()
+                    .setInStock(true)
+                    .setPrice(temp.getPrice())
+                    .build();
+        } catch (Exception e) {
+            System.out.println("ERROR: " + e.getMessage());
+            response = ProductResponse.newBuilder()
+                        .setInStock(false)
+                    .setPrice(0).build();
+        }
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}

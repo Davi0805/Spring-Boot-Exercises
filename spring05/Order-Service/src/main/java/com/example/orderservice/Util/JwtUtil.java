a43c1d5ffa350c4ca3d4401d42e5e11b6cb746b2
Davi0805 <melocardozo.video@gmail.com>
2025-03-18 23:17:36 +0000

    feat(Order-Service/Jwt): jwt dependencies and jwt utils

---
+++ spring05/Order-Service/src/main/java/com/example/orderservice/Util/JwtUtil.java
@@ -0,0 +1,61 @@
+package com.example.orderservice.Util;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+@Component
+public class JwtUtil {
+    @Value("${jwt.secret}")
+    private String token;
+
+    private SecretKey key;
+
+    @Value("${jwt.expiration}")
+    private int expirationTime;
+
+
+    @PostConstruct
+    public void init() {
+        this.key = Keys.hmacShaKeyFor(token.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String generateToken(String userId) {
+        return Jwts.builder()
+                .setSubject(userId)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + expirationTime))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public boolean validateJwtToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (SecurityException e) {
+            System.out.println("Invalid JWT signature: " + e.getMessage());
+        } catch (MalformedJwtException e) {
+            System.out.println("Invalid JWT token: " + e.getMessage());
+        } catch (ExpiredJwtException e) {
+            System.out.println("JWT token is expired: " + e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            System.out.println("JWT token is unsupported: " + e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println("JWT claims string is empty: " + e.getMessage());
+        }
+        return false;
+    }
+
+    public String getUUIDFromToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key).build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+}

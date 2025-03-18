ad71dd6a26b1874a72528e6da032ad6057c4ceb9
Davi0805 <melocardozo.video@gmail.com>
2025-03-18 16:56:53 +0000

    feat(product-service/GrpcMessageService): updateStock method created but still need the Repository Custom Query to decrement

--- spring05/product-service/src/main/java/com/example/productservice/Grpc/GrpcMessageService.java
+++ spring05/product-service/src/main/java/com/example/productservice/Grpc/GrpcMessageService.java
@@ -1,8 +1,6 @@
 package com.example.productservice.Grpc;
 
-import com.example.grpc.product.ProductRequest;
-import com.example.grpc.product.ProductResponse;
-import com.example.grpc.product.ProductServiceGrpc;
+import com.example.grpc.product.*;
 import com.example.productservice.Models.Product;
 import com.example.productservice.Repository.ProductRepository;
 import io.grpc.stub.StreamObserver;
@@ -17,18 +15,16 @@ public class GrpcMessageService extends ProductServiceGrpc.ProductServiceImplBas
     private final ProductRepository cursor;
 
     @Autowired
-    public GrpcMessageService(ProductRepository repo)
-    {
+    public GrpcMessageService(ProductRepository repo) {
         this.cursor = repo;
     }
 
     @Override
-    public void checkStock(ProductRequest request, StreamObserver<ProductResponse> responseObserver)
-    {
+    public void checkStock(ProductRequest request, StreamObserver<ProductResponse> responseObserver) {
         ProductResponse response;
         try {
             Product temp = cursor.findById(UUID.fromString(request.getProductId()))
-                                .orElseThrow(() -> new RuntimeException("Produto nao encontrado"));
+                    .orElseThrow(() -> new RuntimeException("Produto nao encontrado"));
 
             if (temp.getQuantity() < request.getQuantity() && request.getQuantity() > 0)
                 throw new RuntimeException("Estoque em falta");
@@ -40,8 +36,31 @@ public class GrpcMessageService extends ProductServiceGrpc.ProductServiceImplBas
         } catch (Exception e) {
             System.out.println("ERROR: " + e.getMessage());
             response = ProductResponse.newBuilder()
-                        .setInStock(false)
+                    .setInStock(false)
                     .setPrice(0).build();
+        }
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void updateStock(OrderRequest request, StreamObserver<OrderResponse> responseObserver)
+    {
+        OrderResponse response;
+        String orderId;
+        try {
+
+            for (ProductResponse temp : request.getProductsList())
+            {
+                // TODO: QUERY DE DECREMENTO NO PRODUCT REPOSITORY
+            }
+
+            response = OrderResponse.newBuilder().setSuccess(true).build();
+
+        } catch (Exception e) {
+            System.out.println("ERRO: " + e.getMessage());
+            response = OrderResponse.newBuilder().setSuccess(false).build();
         }
 
         responseObserver.onNext(response);

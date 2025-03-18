30d02e9069f872ace3682ae871515aa7ff695a08
Davi0805 <melocardozo.video@gmail.com>
2025-03-18 02:30:05 +0000

    feat(user-service/Config): Redis config

---
+++ spring05/user-service/src/main/java/com/example/userservice/Config/RedisConfig.java
@@ -0,0 +1,27 @@
+package com.example.userservice.Config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.redis")
+    JedisConnectionFactory jedisConnectionFactory() {
+        return new JedisConnectionFactory();
+    }
+
+    @Bean
+    @Primary
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        return template;
+    }
+}

2e2bd22f324953a0fdabe5711f62194801a0cc81
Davi0805 <melocardozo.video@gmail.com>
2025-03-18 03:22:26 +0000

    feat(user-service/DTO): FrontEndMetadataDTO modified to return userId and nome

--- spring05/user-service/src/main/java/com/example/userservice/DTO/FrontEndMetadataDTO.java
+++ spring05/user-service/src/main/java/com/example/userservice/DTO/FrontEndMetadataDTO.java
@@ -5,11 +5,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.UUID;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 public class FrontEndMetadataDTO {
+    private UUID id;
     private String nome;
-    private String sobrenome;
 }

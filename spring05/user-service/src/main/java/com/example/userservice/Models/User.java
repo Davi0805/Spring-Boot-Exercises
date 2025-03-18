1891daff2329eece3aaaec4692e2899c5dcb9e83
Davi0805 <melocardozo.video@gmail.com>
2025-03-18 01:40:19 +0000

    feat(user-service/Models): User Model Created

---
+++ spring05/user-service/src/main/java/com/example/userservice/Models/User.java
@@ -0,0 +1,42 @@
+package com.example.userservice.Models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Table(name = "users")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String sobrenome;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @CreatedDate
+    private Date created_at;
+}

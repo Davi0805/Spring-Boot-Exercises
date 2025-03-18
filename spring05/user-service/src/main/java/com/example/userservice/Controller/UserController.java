73a14eca3b99b5f179bed4fb249c31022d914400
Davi0805 <melocardozo.video@gmail.com>
2025-03-18 03:24:17 +0000

    feat(user-service/Controller): createUser and login endpoints implemented

---
+++ spring05/user-service/src/main/java/com/example/userservice/Controller/UserController.java
@@ -0,0 +1,53 @@
+package com.example.userservice.Controller;
+
+import com.example.userservice.DTO.FrontEndMetadataDTO;
+import com.example.userservice.DTO.LoginDTO;
+import com.example.userservice.Models.User;
+import com.example.userservice.Redis.JwtSession;
+import com.example.userservice.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    // TODO: LOGICA ROBUSTA DE LOG
+
+    private final UserService service;
+
+    @Autowired
+    public UserController(UserService user)
+    {
+        this.service = user;
+    }
+
+    @PostMapping
+    public ResponseEntity<FrontEndMetadataDTO> login(@RequestBody LoginDTO req)
+    {
+        try {
+            return ResponseEntity.ok(service.login(req));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createUser(User req)
+    {
+        try {
+            service.createUser(req);
+            return ResponseEntity.ok("User criado com sucesso!");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Erro: Falha ao criar usuario");
+        }
+    }
+}

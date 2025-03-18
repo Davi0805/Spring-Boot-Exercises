aa47b1b6782c3cc8e23bd458e68e6c557cfa800e
Davi0805 <melocardozo.video@gmail.com>
2025-03-18 23:21:06 +0000

    feat(Order-Service/Services): Order Service created

---
+++ spring05/Order-Service/src/main/java/com/example/orderservice/Service/OrderService.java
@@ -0,0 +1,27 @@
+package com.example.orderservice.Service;
+
+import com.example.grpc.product.OrderResponse;
+import com.example.orderservice.Models.Order;
+import com.example.orderservice.Repository.OrderRepository;
+import jakarta.persistence.Id;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class OrderService {
+
+    private final OrderRepository cursor;
+
+    @Autowired
+    public OrderService(OrderRepository repo)
+    {
+        this.cursor = repo;
+    }
+
+    public void createOrder(Order order)
+    {
+        cursor.save(order);
+    }
+}

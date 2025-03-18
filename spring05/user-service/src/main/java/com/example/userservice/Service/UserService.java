0d75cf83b944716226bdfe5daa503b6804246e8c
Davi0805 <melocardozo.video@gmail.com>
2025-03-18 05:07:19 +0000

    feat(user-service/Service): dto created to Profile/Personal information page

--- spring05/user-service/src/main/java/com/example/userservice/Service/UserService.java
+++ spring05/user-service/src/main/java/com/example/userservice/Service/UserService.java
@@ -2,6 +2,7 @@ package com.example.userservice.Service;
 
 import com.example.userservice.DTO.FrontEndMetadataDTO;
 import com.example.userservice.DTO.LoginDTO;
+import com.example.userservice.DTO.PersonalDataDTO;
 import com.example.userservice.Models.User;
 import com.example.userservice.Redis.JwtSession;
 import com.example.userservice.Repository.JwtSessionRepository;
@@ -10,7 +11,9 @@ import com.example.userservice.Util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Objects;
+import java.util.UUID;
 
 @Service
 public class UserService {
@@ -20,20 +23,17 @@ public class UserService {
     private final JwtUtil jwt;
 
     @Autowired
-    private UserService(UserRepository repository, JwtSessionRepository redis, JwtUtil jwt)
-    {
+    private UserService(UserRepository repository, JwtSessionRepository redis, JwtUtil jwt) {
         this.cursor = repository;
         this.redisCursor = redis;
         this.jwt = jwt;
     }
 
-    public void createUser(User req)
-    {
+    public void createUser(User req) {
         cursor.save(req);
     }
 
-    public FrontEndMetadataDTO login(LoginDTO req) throws RuntimeException
-    {
+    public FrontEndMetadataDTO login(LoginDTO req) throws RuntimeException {
         // AUTH
         User tempUser = cursor.findByEmail(req.getEmail())
                 .orElseThrow(() -> new RuntimeException("Email ou senha nao encontrado!"));
@@ -45,5 +45,16 @@ public class UserService {
         redisCursor.save(session);
 
         return new FrontEndMetadataDTO(tempUser.getId(), tempUser.getNome(), session.getId());
+    }
+
+    public PersonalDataDTO getMyUser(String token) throws RuntimeException
+    {
+        JwtSession session = redisCursor.findById(token).orElseThrow(() -> new RuntimeException("Sessao invalida!"));
+        if (!jwt.validateJwtToken(token))
+            throw new RuntimeException("Sessao invalida!");
+
+        UUID id = UUID.fromString(jwt.getUUIDFromToken(token));
+
+        return new PersonalDataDTO(cursor.findById(id).orElseThrow(() -> new RuntimeException("Usuario nao encontrado!")));
     }
 }

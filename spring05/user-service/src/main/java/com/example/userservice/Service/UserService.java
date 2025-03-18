6668b58840245adb5b89e78f01fc249ad33fc6b8
Davi0805 <melocardozo.video@gmail.com>
2025-03-18 03:23:47 +0000

    feat(user-service/Service): createUser and login methods implemented

--- spring05/user-service/src/main/java/com/example/userservice/Service/UserService.java
+++ spring05/user-service/src/main/java/com/example/userservice/Service/UserService.java
@@ -3,7 +3,10 @@ package com.example.userservice.Service;
 import com.example.userservice.DTO.FrontEndMetadataDTO;
 import com.example.userservice.DTO.LoginDTO;
 import com.example.userservice.Models.User;
+import com.example.userservice.Redis.JwtSession;
+import com.example.userservice.Repository.JwtSessionRepository;
 import com.example.userservice.Repository.UserRepository;
+import com.example.userservice.Util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,11 +16,15 @@ import java.util.Objects;
 public class UserService {
 
     private final UserRepository cursor;
+    private final JwtSessionRepository redisCursor;
+    private final JwtUtil jwt;
 
     @Autowired
-    private UserService(UserRepository repository)
+    private UserService(UserRepository repository, JwtSessionRepository redis, JwtUtil jwt)
     {
         this.cursor = repository;
+        this.redisCursor = redis;
+        this.jwt = jwt;
     }
 
     public void createUser(User req)
@@ -25,14 +32,18 @@ public class UserService {
         cursor.save(req);
     }
 
-    public FrontEndMetadataDTO login(LoginDTO req)
+    public FrontEndMetadataDTO login(LoginDTO req) throws RuntimeException
     {
+        // AUTH
         User tempUser = cursor.findByEmail(req.getEmail())
                 .orElseThrow(() -> new RuntimeException("Email ou senha nao encontrado!"));
-
         if (!Objects.equals(req.getPassword(), tempUser.getPassword()))
             throw new RuntimeException("Email ou senha nao encontrado!");
 
-        return new FrontEndMetadataDTO(tempUser.getNome(), tempUser.getSobrenome());
+        // JWT - SALVA JWT E SESSION INFO EM CACHE
+        JwtSession session = new JwtSession(jwt.generateToken(String.valueOf(tempUser.getId())), tempUser.getNome());
+        redisCursor.save(session);
+
+        return new FrontEndMetadataDTO(tempUser.getId(), tempUser.getNome());
     }
 }

5b08e0a9ad1e5aca06525aa437a20c9a29a6c4d6
Davi0805 <melocardozo.video@gmail.com>
2025-03-19 00:12:01 +0000

    feat(Order-Service/Controller): Unorganized OrderController

--- spring05/Order-Service/src/main/java/com/example/orderservice/Controller/OrderController.java
+++ spring05/Order-Service/src/main/java/com/example/orderservice/Controller/OrderController.java
@@ -1,14 +1,22 @@
 package com.example.orderservice.Controller;
 
+import com.example.grpc.product.ProductResponse;
+import com.example.orderservice.Models.Order;
+import com.example.orderservice.Models.OrderStatus;
+import com.example.orderservice.Models.Product;
+import com.example.orderservice.OrderInDTO;
 import com.example.orderservice.Redis.JwtSession;
 import com.example.orderservice.Repository.JwtSessionRepository;
+import com.example.orderservice.Service.OrderService;
+import com.example.orderservice.Service.grpcOrderClient;
+import com.example.orderservice.Util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
+
+import java.util.List;
+import java.util.UUID;
 
 import static org.springframework.web.servlet.function.ServerResponse.badRequest;
 
@@ -19,27 +27,56 @@ public class OrderController {
     private static final String BEARER_PREFIX = "Bearer ";
 
     private final JwtSessionRepository redisCursor;
+    private final grpcOrderClient grpcClient;
+    private final JwtUtil jwt;
+    private final OrderService cursor;
 
     @Autowired
-    public OrderController(JwtSessionRepository redisCursor) {
+    public OrderController(JwtSessionRepository redisCursor, grpcOrderClient client,
+                            JwtUtil jwt, OrderService orderCursor) {
+        this.grpcClient = client;
         this.redisCursor = redisCursor;
+        this.jwt = jwt;
+        this.cursor = orderCursor;
     }
 
-    @GetMapping
-    public ResponseEntity<?> getOrders(@RequestHeader("Authorization") String token) {
-        System.out.println(token);
+    @PostMapping
+    public ResponseEntity<?> createOrder(@RequestHeader("Authorization") String token,
+                                       @RequestBody OrderInDTO order) {
         token = extractToken(token);
-        System.out.println(token);
         JwtSession session;
-
+        Order req = new Order(order.getItems());
         try {
             session = redisCursor.findById(token).orElseThrow(() -> new RuntimeException("Token não encontrado"));
+            if (!jwt.validateJwtToken(session.getId()))
+                throw new RuntimeException("Falha ao se autenticar!");
+
+            UUID userId = UUID.fromString(jwt.getUUIDFromToken(session.getId()));
+            double totalPrice = 0;
+            List<Product> produtos = order.getItems();
+
+            for (Product prodIt : produtos)
+            {
+                ProductResponse response = grpcClient.checkStock(String.valueOf(prodIt.getId()), prodIt.getQuantity());
+                if (!response.getInStock())
+                    throw new RuntimeException("Produto " + prodIt.getId() + " sem estoque dessa quantidade!");
+                totalPrice += response.getPrice();
+            }
+
+            if (!grpcClient.updateStock(String.valueOf(req.getId()), produtos))
+                throw new RuntimeException("Erro ao atualizar inventario!");
+
+            req.setStatus(OrderStatus.PENDING);
+            req.setUserId(userId);
+            req.setTotalPrice(totalPrice);
+
+            cursor.createOrder(req);
         } catch (Exception e) {
-            System.out.println("Erro ao buscar token");
-            return ResponseEntity.badRequest().body("Erro ao buscar token");
+            System.out.println("ERRO:" + e.getMessage());
+            return ResponseEntity.badRequest().body("ERRO:" + e.getMessage());
         }
 
-        return ResponseEntity.ok(session);
+        return ResponseEntity.ok().build();
     }
 
     private String extractToken(String bearerToken) {

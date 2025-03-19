68b5a60abe92c5ad8ae152745b3fe98150f687bd
Davi0805 <melocardozo.video@gmail.com>
2025-03-19 18:29:07 +0000

    fix(OrderService/RedisConfig): Fixing the compatibility of redis in docker compose env. A lot of problem just to work out of my own laptop :|

--- spring05/Order-Service/src/main/java/com/example/orderservice/Config/RedisConfig.java
+++ spring05/Order-Service/src/main/java/com/example/orderservice/Config/RedisConfig.java
@@ -1,10 +1,13 @@
 package com.example.orderservice.Config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisKeyValueAdapter;
 import org.springframework.data.redis.core.RedisKeyValueTemplate;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -18,17 +21,29 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
 @EnableRedisRepositories(basePackages = "com.example.orderservice.Repository")
 public class RedisConfig {
 
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
     @Bean
     @Primary
-    @ConfigurationProperties(prefix = "spring.redis")
-    JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
+    //@ConfigurationProperties(prefix = "spring.redis")
+    public LettuceConnectionFactory jedisConnectionFactory() {
+
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(redisHost);
+        config.setPort(redisPort);
+        return new LettuceConnectionFactory(config);
+
     }
 
     @Bean
     @Primary
     public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setEnableTransactionSupport(true);
         template.setConnectionFactory(jedisConnectionFactory());
         return template;
     }
@@ -38,6 +53,7 @@ public class RedisConfig {
         RedisMappingContext mappingContext = new RedisMappingContext();
         RedisTypeMapper typeMapper = new DefaultRedisTypeMapper(null);
         MappingRedisConverter converter = new MappingRedisConverter(mappingContext, null, null);
+        converter.afterPropertiesSet();
         return new RedisKeyValueTemplate(new RedisKeyValueAdapter(redisTemplate), mappingContext);
     }
 }

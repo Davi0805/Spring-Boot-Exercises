d6de4efd747b49e240fb4cfccc00165c483e54d3
Davi0805 <melocardozo.video@gmail.com>
2025-03-20 14:16:16 +0000

    feat(payment-service/Kafka): newOrderListener now desserialize the message to KafkaOrderMessageDTO

--- spring05/payment-service/src/main/java/com/example/payment_service/Kafka/KafkaConsumer.java
+++ spring05/payment-service/src/main/java/com/example/payment_service/Kafka/KafkaConsumer.java
@@ -1,5 +1,7 @@
 package com.example.payment_service.Kafka;
 
+import com.example.payment_service.DTO.KafkaOrderMessageDTO;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -20,6 +22,15 @@ public class KafkaConsumer {
     @KafkaListener(topics = "orders", groupId = "order-group")
     public void newOrderListener(String message)
     {
+        KafkaOrderMessageDTO orderDetails;
         System.out.println("Mensagem consumida: " + message);
+        try {
+            orderDetails = desserializer.readValue(message, KafkaOrderMessageDTO.class);
+        } catch (JsonProcessingException e) {
+            System.out.println("ERRO: Falha ao desserializar Kafka order message");
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Mensagem desserializada: " + orderDetails);
     }
 }

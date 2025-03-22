8ada9eb3633bd810493f6473552ef2e643ac1c7f
Davi0805 <melocardozo.video@gmail.com>
2025-03-22 16:54:47 +0000

    feat(payment-service/Service): Service created but not implemented cause to store properly payment entity we need to receive other fields via gRPC

---
+++ spring05/payment-service/src/main/java/com/example/payment_service/Service/PaymentService.java
@@ -0,0 +1,35 @@
+package com.example.payment_service.Service;
+
+import com.example.payment_service.Models.Payment;
+import com.example.payment_service.Models.PaymentMethod;
+import com.example.payment_service.Repository.PaymentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class PaymentService {
+
+    private final PaymentRepository cursor;
+
+    @Autowired
+    public PaymentService(PaymentRepository service) {
+        this.cursor = service;
+    }
+
+    @Async
+    public void createPaymentRegister(Payment paymentDetails) {
+        cursor.save(paymentDetails);
+    }
+
+    public void setPaymentSuccess(UUID orderId, boolean success)
+    {
+        Payment paymentDetails = cursor.findPaymentByOrderId(orderId)
+                .orElseThrow(() -> new RuntimeException("Falha ao encontrar pagamento!"));
+
+        paymentDetails.setSuccess(success);
+        cursor.save(paymentDetails);
+    }
+}

6fe6e1c85ec9e214b806c3b0d2be31f1a8939c72
Davi0805 <melocardozo.video@gmail.com>
2025-03-24 13:22:15 +0000

    feat(payment-service/WebHook): Added paymentService to change the status of payment when receive a callback

--- spring05/payment-service/src/main/java/com/example/payment_service/Controller/PaymentWebHooks.java
+++ spring05/payment-service/src/main/java/com/example/payment_service/Controller/PaymentWebHooks.java
@@ -1,7 +1,10 @@
 package com.example.payment_service.Controller;
 
 import com.example.payment_service.DTO.KafkaPaymentMessageDTO;
+import com.example.payment_service.Models.Payment;
 import com.example.payment_service.Service.KafkaProducer;
+import com.example.payment_service.Service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -11,15 +14,19 @@ import java.util.UUID;
 public class PaymentWebHooks {
 
     private final KafkaProducer kafka;
+    private final PaymentService cursor;
 
-    public PaymentWebHooks(KafkaProducer kafkaProd)
+    @Autowired
+    public PaymentWebHooks(KafkaProducer kafkaProd, PaymentService cursor)
     {
         this.kafka = kafkaProd;
+        this.cursor = cursor;
     }
 
     @GetMapping("/{orderId}")
     public void successPayment(@PathVariable UUID orderId) {
         System.out.println("CALLBACK DE SUCESSO DO PEDIDO: " + orderId);
+        cursor.setPaymentSuccess(orderId, true);
         kafka.sendMessage("orderPayments", new KafkaPaymentMessageDTO(orderId, true).toString());
     }
 
@@ -27,6 +34,7 @@ public class PaymentWebHooks {
     public void failedPayment(@PathVariable UUID orderId)
     {
         System.out.println("CALLBACK DE FALHA DO PEDIDO: " + orderId);
+        cursor.setPaymentSuccess(orderId, false);
         kafka.sendMessage("orderPayments", new KafkaPaymentMessageDTO(orderId, false).toString());
     }
 }

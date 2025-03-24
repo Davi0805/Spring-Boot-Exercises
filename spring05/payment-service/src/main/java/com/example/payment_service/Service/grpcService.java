03ab9a3c17b3475af65a46c5ad0e7fd029a8a452
Davi0805 <melocardozo.video@gmail.com>
2025-03-24 13:19:13 +0000

    feat(payment-service/gRPC): Payments logic to save data about it, maybe for invoice or control

--- spring05/payment-service/src/main/java/com/example/payment_service/Service/grpcService.java
+++ spring05/payment-service/src/main/java/com/example/payment_service/Service/grpcService.java
@@ -2,19 +2,25 @@ package com.example.payment_service.Service;
 
 import com.example.grpc.product.*;
 import com.example.payment_service.DTO.GrpcOrderMessage;
+import com.example.payment_service.Models.Payment;
+import com.example.payment_service.Models.PaymentMethod;
 import com.stripe.exception.StripeException;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @GrpcService
 public class grpcService extends ProductServiceGrpc.ProductServiceImplBase {
 
     private final StripePaymentService paymentService;
+    private final PaymentService paymentCursor;
 
-    public grpcService(StripePaymentService paymentService)
+    public grpcService(StripePaymentService paymentService, PaymentService paymentCursor)
     {
         this.paymentService = paymentService;
+        this.paymentCursor = paymentCursor;
     }
 
     @Override
@@ -22,9 +28,19 @@ public class grpcService extends ProductServiceGrpc.ProductServiceImplBase {
     {
         String paymentLink;
         OrderPaymentLink response;
+        double totalPrice = 0;
+
+        for(ProductPaymentRequest product : request.getProductsList())
+            totalPrice += product.getUnitPrice() * product.getQuantity();
 
         try {
             paymentLink = createPaymentLink(request);
+
+            Payment register = new Payment(UUID.fromString(request.getOrderId()),
+                    totalPrice, PaymentMethod.CREDIT_CARD);
+
+            paymentCursor.createPaymentRegister(register);
+
             response = OrderPaymentLink.newBuilder()
                     .setSuccess(true)
                     .setPaymentLink(paymentLink)

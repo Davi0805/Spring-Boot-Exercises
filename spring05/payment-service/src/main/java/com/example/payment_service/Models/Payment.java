fc8789681bc5e21f83b39fb04b7fe5733b73cde9
Davi0805 <melocardozo.video@gmail.com>
2025-03-24 13:16:45 +0000

    feat(payment-service/Model): Custom payment constructor added

--- spring05/payment-service/src/main/java/com/example/payment_service/Models/Payment.java
+++ spring05/payment-service/src/main/java/com/example/payment_service/Models/Payment.java
@@ -1,5 +1,6 @@
 package com.example.payment_service.Models;
 
+import com.example.payment_service.Service.PaymentService;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -18,15 +19,17 @@ public class Payment {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
-
     private UUID orderId;
-
     private UUID userId;
-
     private double totalPrice;
-
     @Enumerated(EnumType.STRING)
     private PaymentMethod paymentMethod;
-
     private boolean success;
+
+    public Payment(UUID orderId, double totalPrice, PaymentMethod method)
+    {
+        this.orderId = orderId;
+        this.totalPrice = totalPrice;
+        this.paymentMethod = method;
+    }
 }

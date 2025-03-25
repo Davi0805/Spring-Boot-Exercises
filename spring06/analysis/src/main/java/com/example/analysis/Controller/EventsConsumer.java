989cc101e5477067ac725be05bbc3a40dc7ffd09
Davi0805 <melocardozo.video@gmail.com>
2025-03-25 12:44:42 +0000

    feat(Analysis/Controller): TransactionConsumer created, added desserializer and prints for debug during development

---
+++ spring06/analysis/src/main/java/com/example/analysis/Controller/EventsConsumer.java
@@ -0,0 +1,51 @@
+package com.example.analysis.Controller;
+
+import com.example.analysis.DTO.TransactionDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+// Regras a ser implementado para gerar alerta
+// 1 - Checar transacao duplicada com
+// range de 1-5 min
+// 2 - Transacoes acima de 2000
+// 3 -  picos (Nao sei oq seriam esses picos ainda)
+
+
+@Component
+public class EventsConsumer {
+
+    private final ObjectMapper desserializer;
+
+    @Autowired
+    public EventsConsumer(ObjectMapper desserializer)
+    {
+        this.desserializer = desserializer;
+    }
+
+    @KafkaListener(topics = "transactions", groupId = "data-science")
+    public void Transactions(String message)
+    {
+        System.out.println("MENSAGEM CONSUMIDA: " + message);
+        try {
+            TransactionDTO transaction = desserializer
+                    .readValue(message, TransactionDTO.class);
+
+            System.out.println("MENSAGEM DESSERIALIZADA: id = "
+                    + transaction.getId() + " | userId = "
+                    + transaction.getUser_id()
+                    + " | amount = "
+                    + transaction.getAmount()
+                    + " | timestamp = "
+                    + transaction.getTimestamp()
+                    + " | location = "
+                    + transaction.getLocation());
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
+// {"id": "0d1e4ed6-fc62-4e4b-a3c3-b2a98321adb7","user_id": "a2e20537-bfe3-42e3-8c63-0a4796bc6a6c","amount": 1500.00,"timestamp": "2024-03-24T12:00:00Z","location": "São Paulo, BR"}

13d0e90c383aa9a9de36a2381333d907511d5378
Davi0805 <melocardozo.video@gmail.com>
2025-03-25 14:27:25 +0000

    feat(Analysis/Controller): EventsConsumer created to handle all the KafkaConsumer topics

--- spring06/analysis/src/main/java/com/example/analysis/Controller/EventsConsumer.java
+++ spring06/analysis/src/main/java/com/example/analysis/Controller/EventsConsumer.java
@@ -1,6 +1,7 @@
 package com.example.analysis.Controller;
 
 import com.example.analysis.DTO.TransactionDTO;
+import com.example.analysis.Service.TransactionAnalyser;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -17,11 +18,13 @@ import org.springframework.stereotype.Component;
 public class EventsConsumer {
 
     private final ObjectMapper desserializer;
+    private final TransactionAnalyser transactionAnalyser;
 
     @Autowired
-    public EventsConsumer(ObjectMapper desserializer)
+    public EventsConsumer(ObjectMapper desserializer, TransactionAnalyser analyser)
     {
         this.desserializer = desserializer;
+        this.transactionAnalyser = analyser;
     }
 
     @KafkaListener(topics = "transactions", groupId = "data-science")
@@ -42,6 +45,8 @@ public class EventsConsumer {
                     + " | location = "
                     + transaction.getLocation());
 
+            transactionAnalyser.run(transaction);
+
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
@@ -49,3 +54,4 @@ public class EventsConsumer {
 }
 
 // {"id": "0d1e4ed6-fc62-4e4b-a3c3-b2a98321adb7","user_id": "a2e20537-bfe3-42e3-8c63-0a4796bc6a6c","amount": 1500.00,"timestamp": "2024-03-24T12:00:00Z","location": "São Paulo, BR"}
+// {"id": "0d1e4ed6-fc62-4e4b-a3c3-b2a98321adb7","user_id": "a1e20537-bfe3-42e3-8c63-0a4796bc6a6c","amount": 2004.00,"timestamp": "2024-03-24T12:00:00Z","location": "São Paulo, BR"}

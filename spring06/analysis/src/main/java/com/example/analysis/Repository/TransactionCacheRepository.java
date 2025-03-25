342de5f1d5fc32b5248aac8d1c5238e7463fb7a2
Davi0805 <melocardozo.video@gmail.com>
2025-03-25 14:24:31 +0000

    feat(Analysis/Repository): findByUserIdAndAmout method added to TransactionCacheRepository

--- spring06/analysis/src/main/java/com/example/analysis/Repository/TransactionCacheRepository.java
+++ spring06/analysis/src/main/java/com/example/analysis/Repository/TransactionCacheRepository.java
@@ -5,8 +5,12 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.RepositoryDefinition;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface TransactionCacheRepository extends CrudRepository<CachedTransaction, UUID> {
+
+    Optional<CachedTransaction> findByUserIdAndAmount(UUID userId, double amount);
+
 }
